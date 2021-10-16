b1eff8923217617e54ddc611448c6af1c7b12799
IzazRz <73918830+farizaw123@users.noreply.github.com>
2021-10-16 14:50:26 +0700

    Add files via upload

---
+++ 2_XIRPL7_08.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+
+    <title>Belajar LOOP</title>
+
+</head>
+
+<body>
+    
+    <h2>Perualangan</h2>
+    <?php  
+    date_default_timezone_set("asia/jakarta");
+    echo date("d-m-Y") . date(" h:i:sa");
+    echo "<br>";
+    echo "<br>";
+    for($i=0;$i<=5;$i++){  
+    for($j=5-$i;$j>=1;$j--){  
+    echo "* ";  
+    }  
+    echo "<br>";  
+    }  
+?>  
+</body>
---
+++ 3_XIRPL7_08.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<body>
+<?php
+$siswa [0] ['Nama']  = "Satria";
+$siswa [0] ['Kelas']  = "Rpl 1";
+
+$siswa [1] ['Nama']  = "Arsyi";
+$siswa [1] ['Kelas']  = "Rpl 2";
+
+$siswa [2] ['Nama']  = "Dhika";
+$siswa [2] ['Kelas']  = "Rpl 3";
+
+$siswa [3] ['Nama']  = "Evan";
+$siswa [3] ['Kelas']  = "Rpl 4";
+
+$siswa [4] ['Nama']  = "Fabian";
+$siswa [4] ['Kelas']  = "Rpl 5";
+
+$siswa [5] ['Nama']  = "Irfan";
+$siswa [5] ['Kelas']  = "Rpl 6";
+
+
+
+
+echo "<pre>";
+print_r($siswa);
+echo "</pre>";
+ 
+ 
+ ?>
+ </body>
